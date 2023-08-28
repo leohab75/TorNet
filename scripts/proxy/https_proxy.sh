3a9fe140ff072f662fa39278e917ba94353e9acc
leohab75 <73970447+leohab75@users.noreply.github.com>
2023-08-28 16:21:50 +0300

    Update https_proxy.sh
    info

--- scripts/proxy/https_proxy.sh
+++ scripts/proxy/https_proxy.sh
@@ -10,6 +10,7 @@ zenity --notification --text="TorNet \n HTTPs PROXY"
 choice=$(zenity --list --radiolist --window-icon="$icon" --title="Proxy https://" --text="$TorNet \n HTTPS PROXY" --column="🔆" --column="PROXY:PORT" \
     --column="code" --column="flag" "true" "UPDATE" "proxy" "list" $(for i in $(cat https); do
         country_code=$(curl -m 5 -x "$i" ifconfig.io/country_code)
+        zenity --notification --text="TorNet \n Check: $i"
         echo -en "false $i $(bash /usr/local/bin/TorNet/scripts/country_flags.sh $country_code) \n"
     done) --width=350 --height=450)
 
@@ -39,6 +40,7 @@ if [[ "$?" == "0" ]]; then
         buff=$(zenity --list --radiolist --window-icon="$icon" --title="Proxy https://" --text="$TorNet \n Всего: $all_proxy \n " \
             --column="🔆" --column="PROXY:PORT" --column="code" --column="flag" $(for i in $(cat https); do
                 country_code=$(curl -x "$i" -m 5 ifconfig.io/country_code)
+                zenity --notification --text="TorNet \n Check: $i"
                 echo -en "false $i $(bash /usr/local/bin/TorNet/scripts/country_flags.sh $country_code) \n"
                 echo -e "$i  $(bash /usr/local/bin/TorNet/scripts/country_flags.sh $country_code)" >> "$HOME/$Desktop_path/TorNet proxy HTTPs.txt"
             done) --width=350 --height=450)
